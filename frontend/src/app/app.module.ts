c1009f2a8577d69ed406f58cdbf8b0a7448fc681
Erika <brenherrera@alumno.unlam.edu.ar>
2024-04-14 17:01:13 -0300

    saco lo que no servia en app module

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,18 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
-import { RegisterComponent } from './components/register/register.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
-import { SingupComponent } from './components/singup/singup.component';
 import { SignupComponent } from './components/signup/signup.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    RegisterComponent,
     DashboardComponent,
-    SingupComponent,
     SignupComponent
   ],
   imports: [
