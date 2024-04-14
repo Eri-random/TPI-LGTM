3f01e16b0630d293eadc07d4c2e1632f6706fea5
Erika <brenherrera@alumno.unlam.edu.ar>
2024-04-14 15:14:35 -0300

    agrego estructura de carpetas y componentes base limpios

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { SignupComponent } from './components/signup/signup.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'login',component:LoginComponent},
+  {path:'signup',component:SignupComponent},
+  {path:'dashboard',component:DashboardComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
