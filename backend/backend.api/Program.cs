5ca8a33a4a5208d6a1ab2bdeb7d86c11cdc5296c
Cristian <cuehara@alumno.unlam.edu.ar>
2024-04-16 23:47:13 -0300

    agrego end2end cuentas en api

--- backend/backend.api/Program.cs
+++ backend/backend.api/Program.cs
@@ -1,20 +1,36 @@
+using backend.api.Models;
+using backend.data.DataContext;
+using backend.servicios.Interfaces;
+using backend.servicios.Servicios;
+using FluentValidation.AspNetCore;
+using Microsoft.EntityFrameworkCore;
+using System.Text.Json.Serialization;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddControllers().AddJsonOptions(options =>
+{
+    options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+});
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddScoped<IUsuarioService, UsuarioService>();
+builder.Services.AddFluentValidation(fv => fv.RegisterValidatorsFromAssemblyContaining<UsuarioRequestModel>());
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
 
 app.UseHttpsRedirection();
 
---
+++ backend/backend.data/Models/Rol.cs
@@ -0,0 +1,9 @@
+﻿namespace backend.data.Models
+{
+    public class Rol
+    {
+        public int RolId { get; set; }
+        public string Nombre { get; set; }
+        public List<Usuario> Usuarios { get; set; }
+    }
+}
