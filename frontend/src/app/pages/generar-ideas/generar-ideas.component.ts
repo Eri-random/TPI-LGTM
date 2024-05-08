ed67aad64fbf8b5fceaa6cc2cb80a310483bfd83
Tabathapm <tabathapm@gmail.com>
2024-05-07 22:09:01 -0300

    carga de imagenes

--- frontend/src/app/pages/generar-ideas/generar-ideas.component.ts
+++ frontend/src/app/pages/generar-ideas/generar-ideas.component.ts
@@ -6,5 +6,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./generar-ideas.component.css']
 })
 export class GenerarIdeasComponent {
+  imagePreviews: string[] = ['https://media.istockphoto.com/id/1226328537/es/vector/soporte-de-posici%C3%B3n-de-imagen-con-un-icono-de-c%C3%A1mara-gris.jpg?s=612x612&w=0&k=20&c=8igCt_oe2wE-aP0qExUDfwicSNUCb4Ho9DiKCq0rSaA='];
 
+  constructor() { }
+
+  onFileChange(event: any, index: number) {
+    const files = event.target.files;
+
+    if (!files || files.length === 0 || !window.FileReader) return;
+
+    const reader = new FileReader();
+    reader.readAsDataURL(files[0]);
+
+    reader.onloadend = () => {
+      this.imagePreviews[index] = reader.result as string;
+    };
+  }
+
+  addImage() {
+    if (this.imagePreviews.length < 4) {
+      this.imagePreviews.push('https://media.istockphoto.com/id/1226328537/es/vector/soporte-de-posici%C3%B3n-de-imagen-con-un-icono-de-c%C3%A1mara-gris.jpg?s=612x612&w=0&k=20&c=8igCt_oe2wE-aP0qExUDfwicSNUCb4Ho9DiKCq0rSaA=');
+    } else {
+      alert("Solo se permiten hasta 4 imágenes.");
+    }
+  }
+
+  removeImage(index: number) {
+    this.imagePreviews.splice(index, 1);
+  }
 }
