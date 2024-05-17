403686be8b3b688287731a93e87d56a6bd3c8153
Eri-random <74168878+Eri-random@users.noreply.github.com>
2024-05-17 15:41:47 -0300

    Feature/reconocimiento tela (#23)
    * agrego modelo de reconocimiento de imagenes para clasificar tipo de tela
    
    * actualizo imageFiles cuando elimino imagen
    
    * reconocimiento de idea momentaneo con una sola imagen

---
+++ backend/backend.api/Models/FabricDataModel.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.ML.Data;
+
+namespace backend.api.Models
+{
+    public class FabricModelInput
+    {
+        [LoadColumn(0)]
+        [ColumnName(@"Label")]
+        public string Label { get; set; }
+
+        [LoadColumn(1)]
+        [ColumnName(@"ImageSource")]
+        public byte[] ImageSource { get; set; }
+
+    }
+
+    public class FabricModelOutput
+    {
+        [ColumnName(@"Label")]
+        public uint Label { get; set; }
+
+        [ColumnName(@"ImageSource")]
+        public byte[] ImageSource { get; set; }
+
+        [ColumnName(@"PredictedLabel")]
+        public string PredictedLabel { get; set; }
+
+        [ColumnName(@"Score")]
+        public float[] Score { get; set; }
+
+    }
+}
---
+++ frontend/src/app/services/reconocimiento-tela.service.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environments } from '../environments/environments';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ReconocimientoTelaService {
+
+  private baseUrl = environments.baseUrl;
+
+  constructor(private http: HttpClient) { }
+
+  classifyImage(image: File):Observable<any> {
+    const formData = new FormData();
+    formData.append("image", image, image.name);
+
+    return this.http.post(`${this.baseUrl}/FabricClassification`, formData);
+  }
+}
