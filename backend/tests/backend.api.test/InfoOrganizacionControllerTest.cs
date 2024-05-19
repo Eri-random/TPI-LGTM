c0c12b9c8151496768cd2b06dbd3931e61ed4a64
Yair Oliva <yairolivait@gmail.com>
2024-05-18 21:59:46 -0300

    fix test

--- backend/tests/backend.api.test/InfoOrganizacionControllerTest.cs
+++ backend/tests/backend.api.test/InfoOrganizacionControllerTest.cs
@@ -4,11 +4,13 @@ using backend.servicios.DTOs;
 using backend.servicios.Helpers;
 using backend.servicios.Interfaces;
 using Castle.Components.DictionaryAdapter.Xml;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Microsoft.VisualStudio.TestPlatform.CommunicationUtilities;
 using Moq;
 using Newtonsoft.Json.Linq;
+using System.Text;
 
 namespace backend.api.test
 {
@@ -71,7 +73,7 @@ namespace backend.api.test
         }
 
         [Test]
-        public async Task Details_WhenCalled_ReturnsCreated()
+        public async Task Details_WithValidInput_ReturnsCreatedAtActionResult()
         {
             // Arrange
             var infoOrganizacionRequest = new InfoOrganizacionRequest
@@ -80,7 +82,7 @@ namespace backend.api.test
                 Organizacion = "Organizacion",
                 DescripcionBreve = "DescripcionBreve",
                 DescripcionCompleta = "DescripcionCompleta",
-                Img = "Img"
+                File = new FormFile(new MemoryStream(Encoding.UTF8.GetBytes("file content")), 0, "file content".Length, "file", "test.jpg")
             };
 
             var organizacion = new OrganizacionDto
@@ -95,22 +97,14 @@ namespace backend.api.test
             _organizacionService.Setup(x => x.GetOrganizacionByIdAsync(infoOrganizacionRequest.OrganizacionId))
                 .ReturnsAsync(organizacion);
 
-            var infoOrganizacionDto = new InfoOrganizacionDto
-            {
-                Organizacion = infoOrganizacionRequest.Organizacion,
-                DescripcionBreve = infoOrganizacionRequest.DescripcionBreve,
-                DescripcionCompleta = infoOrganizacionRequest.DescripcionCompleta,
-                Img = infoOrganizacionRequest.Img,
-                OrganizacionId = infoOrganizacionRequest.OrganizacionId
-            };
-
-            _organizacionInfoService.Setup(x => x.SaveDataInfoOrganizacion(infoOrganizacionDto)).Returns(Task.CompletedTask);
+            _organizacionInfoService.Setup(x => x.SaveDataInfoOrganizacion(It.IsAny<InfoOrganizacionDto>()))
+                .Returns(Task.CompletedTask);
 
             // Act
             var result = await _controller.Details(infoOrganizacionRequest);
 
             // Assert
-            Assert.IsInstanceOf<CreatedAtActionResult>(result);
+            Assert.That(result, Is.InstanceOf<CreatedAtActionResult>());
 
             var createdResult = result as CreatedAtActionResult;
             Assert.IsNotNull(createdResult);
@@ -118,12 +112,12 @@ namespace backend.api.test
             Assert.AreEqual(1, createdResult.RouteValues["id"]);
             Assert.AreEqual(infoOrganizacionRequest.OrganizacionId, createdResult.RouteValues["id"]);
             Assert.AreEqual(infoOrganizacionRequest.OrganizacionId, (createdResult.Value as InfoOrganizacionDto).OrganizacionId);
-
             Assert.AreEqual(infoOrganizacionRequest.Organizacion, (createdResult.Value as InfoOrganizacionDto).Organizacion);
         }
 
+
         [Test]
-        public async Task Update_WhenCalled_ReturnsOk() 
+        public async Task Update_WhenCalled_ReturnsCreatedAtActionResult()
         {
             // Arrange
             var infoOrganizacionRequest = new InfoOrganizacionRequest
@@ -132,7 +126,7 @@ namespace backend.api.test
                 Organizacion = "Organizacion",
                 DescripcionBreve = "DescripcionBreve",
                 DescripcionCompleta = "DescripcionCompleta",
-                Img = "Img"
+                File = new FormFile(new MemoryStream(Encoding.UTF8.GetBytes("file content")), 0, "file content".Length, "file", "test.jpg")
             };
 
             var organizacion = new OrganizacionDto
@@ -141,37 +135,39 @@ namespace backend.api.test
                 Direccion = "Calle 123",
                 Localidad = "Localidad 1",
                 Provincia = "Provincia 1",
-                Telefono = "123456789"
+                Telefono = "123456789",
+                InfoOrganizacion = new InfoOrganizacionDto
+                {
+                    Img = "http://localhost:5203/images/old_image.jpg"
+                }
             };
+
 
             _organizacionService.Setup(x => x.GetOrganizacionByIdAsync(infoOrganizacionRequest.OrganizacionId))
                 .ReturnsAsync(organizacion);
 
-            var infoOrganizacionDto = new InfoOrganizacionDto
-            {
-                Organizacion = infoOrganizacionRequest.Organizacion,
-                DescripcionBreve = infoOrganizacionRequest.DescripcionBreve,
-                DescripcionCompleta = infoOrganizacionRequest.DescripcionCompleta,
-                Img = infoOrganizacionRequest.Img,
-                OrganizacionId = infoOrganizacionRequest.OrganizacionId
-            };
+            _organizacionInfoService.Setup(x => x.UpdateInfoOrganizacionAsync(It.IsAny<InfoOrganizacionDto>()))
+                .Returns(Task.CompletedTask);
 
-            _organizacionInfoService.Setup(x => x.UpdateInfoOrganizacionAsync(infoOrganizacionDto)).Returns(Task.CompletedTask);
 
             // Act
             var result = await _controller.Update(infoOrganizacionRequest);
 
             // Assert
-
             Assert.IsInstanceOf<CreatedAtActionResult>(result);
 
             var createdResult = result as CreatedAtActionResult;
             Assert.IsNotNull(createdResult);
-
             Assert.AreEqual("Update", createdResult.ActionName);
-            Assert.AreEqual(1, createdResult.RouteValues["id"]);
             Assert.AreEqual(infoOrganizacionRequest.OrganizacionId, createdResult.RouteValues["id"]);
 
+            var returnedValue = createdResult.Value as InfoOrganizacionDto;
+            Assert.IsNotNull(returnedValue);
+            Assert.AreEqual(infoOrganizacionRequest.OrganizacionId, returnedValue.OrganizacionId);
+            Assert.AreEqual(infoOrganizacionRequest.Organizacion, returnedValue.Organizacion);
+            Assert.AreEqual(infoOrganizacionRequest.DescripcionBreve, returnedValue.DescripcionBreve);
+            Assert.AreEqual(infoOrganizacionRequest.DescripcionCompleta, returnedValue.DescripcionCompleta);
+            Assert.AreEqual("http://localhost:5203/images/test.jpg", returnedValue.Img);
         }
 
         [Test]
@@ -184,7 +180,7 @@ namespace backend.api.test
                 Organizacion = "Organizacion",
                 DescripcionBreve = "DescripcionBreve",
                 DescripcionCompleta = "DescripcionCompleta",
-                Img = "Img"
+                File = new FormFile(new MemoryStream(Encoding.UTF8.GetBytes("file content")), 0, "file content".Length, "file", "test.jpg")
             };
 
             var organizacion = new OrganizacionDto
@@ -204,7 +200,7 @@ namespace backend.api.test
                 Organizacion = infoOrganizacionRequest.Organizacion,
                 DescripcionBreve = infoOrganizacionRequest.DescripcionBreve,
                 DescripcionCompleta = infoOrganizacionRequest.DescripcionCompleta,
-                Img = infoOrganizacionRequest.Img,
+                Img = infoOrganizacionRequest.File.FileName,
                 OrganizacionId = infoOrganizacionRequest.OrganizacionId
             };
 
