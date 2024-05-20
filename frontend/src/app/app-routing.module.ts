d44289121e648a7eb56285f987854c1ab3ee4760
Tabatha <49240387+Tabathapm@users.noreply.github.com>
2024-05-20 20:10:58 -0300

    Feature/info organizacion (#31)
    * component info-organizacion
    
    * vista de info organización
    
    * modificacion del footer
    
    * modificación de la vista info.organizacion
    
    * fix module
    
    ---------
    
    Co-authored-by: Eri-random <74168878+Eri-random@users.noreply.github.com>
    Co-authored-by: Erika <brenherrera@alumno.unlam.edu.ar>

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -8,9 +8,11 @@ import { MapaOrganizacionesComponent } from './pages/mapa/mapa-organizaciones.co
 import { DonacionesComponent } from './pages/donaciones/donaciones.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { EditInfoComponent } from './pages/dashboard/components/edit-info/edit-info.component';
+import { InfoOrganizacionComponent } from './pages/info-organizacion/info-organizacion.component';
 import { ResponseIdeaComponent } from './pages/generar-ideas/response-idea/response-idea.component';
 import { MisIdeasComponent } from './pages/generar-ideas/mis-ideas/mis-ideas.component';
 import { VerIdeaComponent } from './pages/generar-ideas/mis-ideas/ver-idea/ver-idea.component';
+
 
 
 const routes: Routes = [
@@ -22,6 +24,7 @@ const routes: Routes = [
   {path: 'donar', component:DonacionesComponent},
   {path: 'dashboard', component: DashboardComponent},
   {path: 'mi-organizacion', component: EditInfoComponent},
+  {path: 'info-organizacion', component: InfoOrganizacionComponent},
   {path: 'response-idea', component: ResponseIdeaComponent},
   {path: 'mis-ideas', component: MisIdeasComponent},
   {path: 'mis-ideas/:id', component: VerIdeaComponent},
