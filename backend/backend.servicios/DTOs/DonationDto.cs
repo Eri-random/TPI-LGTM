7431f6455f6ae2541e3cab1846e6609aa9ef922b
cristianuehara <cuehara@alumno.unlam.edu.ar>
2024-05-31 20:38:29 -0300

    trigger ci/cd

--- backend/backend.servicios/DTOs/DonationDto.cs
+++ backend/backend.servicios/DTOs/DonationDto.cs
@@ -9,7 +9,7 @@ namespace backend.servicios.DTOs
 {
     public class DonationDto
     {
-        public int Id { get; set; }
+        public int Id { get; set; } 
 
         public string Producto { get; set; }
 
