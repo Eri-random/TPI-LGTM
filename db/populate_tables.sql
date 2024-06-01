4fdfc8b36d3f1fab497af3a21ff11ed970748b86
c-uehara <166967470+c-uehara@users.noreply.github.com>
2024-06-01 15:56:54 -0300

    CI/CD files (#48)
    * refactor prs validations
    
    * add cd ymls
    
    * minor fix
    
    * trigger ci/cd
    
    * fix path
    
    * add id
    
    * fix package name
    
    * fix package
    
    * fix invalid package
    
    * test
    
    * add missing step
    
    * remove testing triggers
    
    * add api url
    
    * update prod command
    
    * fix environments location
    
    * fix app settings
    
    * trigger
    
    * fix
    
    * fix middleware
    
    * validate changes
    
    * revert
    
    * remove
    
    * test
    
    * trigger
    
    * remove
    
    ---------
    
    Co-authored-by: cristianuehara <cuehara@alumno.unlam.edu.ar>

--- db/populate_tables.sql
+++ db/populate_tables.sql
@@ -4,11 +4,11 @@ INSERT INTO rol (nombre) VALUES
 ('usuario');
 
 -- Insertar tres usuarios nuevos en la tabla 'usuario'
-INSERT INTO usuario (nombre, apellido, email, contrasena, telefono, direccion, localidad, provincia,cuit,rol_id)
+INSERT INTO usuario (nombre, apellido, email, contrasena, telefono, direccion, localidad, provincia,cuit, rol_id)
 VALUES 
-('Nombre1', 'Apellido1', 'test1@test.com', crypt('12345678', gen_salt('bf')), '1234567890', 'Calle Uno 123', 'Ciudad Uno', 'Provincia Uno',null,2),
-('Nombre2', 'Apellido2', 'test2@test.com', crypt('12345678', gen_salt('bf')), '2345678901', 'Calle Dos 234', 'Ciudad Dos', 'Provincia Dos',null,2),
-('Nombre3', 'Apellido3', 'test3@test.com', crypt('12345678', gen_salt('bf')), '3456789012', 'Calle Tres 345', 'Ciudad Tres', 'Provincia Tres',null,2);
+('Nombre1', 'Apellido1', 'test1@test.com', crypt('12345678', gen_salt('bf')), '1234567890', 'Calle Uno 123', 'Ciudad Uno', 'Provincia Uno', 2),
+('Nombre2', 'Apellido2', 'test2@test.com', crypt('12345678', gen_salt('bf')), '2345678901', 'Calle Dos 234', 'Ciudad Dos', 'Provincia Dos', 2),
+('Nombre3', 'Apellido3', 'test3@test.com', crypt('12345678', gen_salt('bf')), '3456789012', 'Calle Tres 345', 'Ciudad Tres', 'Provincia Tres', 2);
 
 INSERT INTO public.necesidad(nombre, icono)
 VALUES ('Ropa y vestimenta', 'http://localhost:5203/iconos/Jumper.png'),
