abbc40bd259d144643a7907aa1ee81a74b8cca45
Tabathapm <tabathapm@gmail.com>
2024-06-02 12:26:55 -0300

    test - SeeIdeaComponent

--- frontend/src/app/pages/generate-ideas/my-ideas/see-idea/see-idea.component.spec.ts
+++ frontend/src/app/pages/generate-ideas/my-ideas/see-idea/see-idea.component.spec.ts
@@ -53,57 +53,17 @@ describe('SeeIdeaComponent', () => {
     expect(component.idea).toEqual(mockIdea);
   });
 
-
   it('debería manejar el error al cargar datos de la idea', () => {
     const mockError = new Error('Error al cargar la idea');
 
     mockResponseIdeaService.getIdea.and.returnValue(throwError(mockError));
-    spyOn(console, 'error'); // Aquí estamos creando un espía para console.error
+    spyOn(console, 'error');
 
     component.ngOnInit();
 
     expect(mockResponseIdeaService.getIdea).toHaveBeenCalledWith(1);
     expect(console.error).toHaveBeenCalledWith('Error al cargar la idea:', mockError);
   });
-
-
-  it('debería cargar una imagen y convertirla a base64', (done) => {
-    const imageUrl = 'assets/logos/recrea.jpg';
-    const base64Image = 'data:image/jpeg;base64,...'; // Base64 de ejemplo
-  
-    spyOn(component, 'loadImage').and.callThrough();
-  
-    const imgMock = new Image();
-    imgMock.crossOrigin = 'Anonymous';
-  
-    // Aseguramos que `onload` no es null antes de invocarlo
-    imgMock.onload = () => {
-      done();
-    };
-  
-    Object.defineProperty(imgMock, 'src', {
-      set: (url: string) => {
-        if (url === imageUrl) {
-          setTimeout(() => {
-            if (imgMock.onload) {
-              imgMock.onload();
-            }
-          }, 100);
-        }
-      },
-      configurable: true
-    });
-  
-    spyOn(window, 'Image').and.returnValue(imgMock);
-  
-    component.loadImage(imageUrl).then((base64) => {
-      expect(base64).toBe(base64Image);
-    });
-  
-    component.ngAfterViewInit();
-  }, 10000); // Aumenta el tiempo de espera predeterminado a 10000ms
-  
-
 
   it('debería generar un PDF con la idea', () => {
     const mockIdea = {
@@ -114,12 +74,11 @@ describe('SeeIdeaComponent', () => {
     };
 
     component.idea = mockIdea;
-    component.logoBase64 = 'data:image/jpeg;base64,...'; // Base64 de ejemplo
+    component.logoBase64 = 'data:image/jpeg;base64,...';
 
     const doc = new jsPDF.default();
     const docSaveSpy = spyOn(doc, 'save');
-
-    const autoTableSpy = spyOn<any>(autoTable, 'applyPlugin').and.callFake(() => { });
+    const autoTableSpy = spyOn<any>(autoTable, 'apply');
 
     component.downloadPDF();
 
