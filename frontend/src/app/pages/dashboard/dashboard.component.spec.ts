330ea965b14a2506246a1ae00ad799ce728eeb74
Tabathapm <tabathapm@gmail.com>
2024-06-02 16:23:36 -0300

    test - DashboardComponent

--- frontend/src/app/pages/dashboard/dashboard.component.spec.ts
+++ frontend/src/app/pages/dashboard/dashboard.component.spec.ts
@@ -1,21 +1,160 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { DashboardComponent } from './dashboard.component';
+import { ComponentFixture, TestBed, fakeAsync, tick } from '@angular/core/testing';
+import { MatDialog } from '@angular/material/dialog';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { MatSort, MatSortModule } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { of, Subject, throwError } from 'rxjs';
+import { DashboardComponent, UserData } from './dashboard.component';
+import { OrganizationService } from 'src/app/services/organization.service';
+import { AuthService } from 'src/app/services/auth.service';
+import { DonationsService } from 'src/app/services/donations.service';
+import { WebsocketService } from 'src/app/services/websocket.service';
+import { NgToastService } from 'ng-angular-popup';
+import { ChangeDetectorRef } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('DashboardComponent', () => {
   let component: DashboardComponent;
   let fixture: ComponentFixture<DashboardComponent>;
+  let organizationServiceMock: any;
+  let authServiceMock: any;
+  let donationsServiceMock: any;
+  let webSocketServiceMock: any;
+  let toastServiceMock: any;
+  let cdrMock: any;
+
+  beforeEach(async () => {
+    organizationServiceMock = jasmine.createSpyObj('OrganizationService', [
+      'getOrgNameFromStore',
+      'getCuitFromStore',
+      'getOrganizationByCuit',
+    ]);
+    authServiceMock = jasmine.createSpyObj('AuthService', [
+      'getOrgNameFromToken',
+      'getCuitFromToken',
+    ]);
+    donationsServiceMock = jasmine.createSpyObj('DonationsService', ['getDonationsByOrganizationId']);
+    webSocketServiceMock = {
+      messages: new Subject(),
+    };
+    toastServiceMock = jasmine.createSpyObj('NgToastService', ['success', 'error']);
+    cdrMock = jasmine.createSpyObj('ChangeDetectorRef', ['detectChanges']);
+
+    await TestBed.configureTestingModule({
+      declarations: [DashboardComponent],
+      imports: [
+        MatPaginatorModule,
+        MatSortModule,
+        BrowserAnimationsModule
+      ],
+      providers: [
+        { provide: OrganizationService, useValue: organizationServiceMock },
+        { provide: AuthService, useValue: authServiceMock },
+        { provide: DonationsService, useValue: donationsServiceMock },
+        { provide: WebsocketService, useValue: webSocketServiceMock },
+        { provide: NgToastService, useValue: toastServiceMock },
+        { provide: ChangeDetectorRef, useValue: cdrMock },
+        { provide: MatDialog, useValue: {} },
+      ],
+    }).compileComponents();
+  });
 
   beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [DashboardComponent]
-    });
     fixture = TestBed.createComponent(DashboardComponent);
     component = fixture.componentInstance;
+    component.paginator = jasmine.createSpyObj('MatPaginator', ['firstPage']);
+    component.sort = jasmine.createSpyObj('MatSort', ['sort']);
+
+    organizationServiceMock.getOrgNameFromStore.and.returnValue(of('Mi Organización'));
+    organizationServiceMock.getCuitFromStore.and.returnValue(of('123456789'));
+    authServiceMock.getOrgNameFromToken.and.returnValue('Mi Organización');
+    authServiceMock.getCuitFromToken.and.returnValue('123456789');
+
+    organizationServiceMock.getOrganizationByCuit.and.returnValue(of({ id: 1 }));
+
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('debería crear el componente', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('debería inicializar los datos de la organización y cargar las donaciones en ngOnInit', () => {
+    const donationsResponse = of([
+      { usuario: { nombre: 'Juan', telefono: '12345678', email: 'juan@ejemplo.com' }, producto: 'Producto A', cantidad: 10 },
+    ]);
+
+    donationsServiceMock.getDonationsByOrganizationId.and.returnValue(donationsResponse);
+
+    component.ngOnInit();
+    fixture.detectChanges();
+
+    expect(component.orgName).toBe('Mi Organización');
+    expect(component.cuit).toBe('123456789');
+    expect(component.existDonations).toBe(true);
+    expect(component.dataSource.data.length).toBe(1);
+    expect(component.totalDonations).toBe(10);
+    expect(component.totalDonationsCount).toBe(1);
+    expect(component.averageDonations).toBe(10);
+    expect(component.productMostDonate).toEqual({ product: 'Producto A', amount: 10 });
+  });
+
+  it('debería manejar nuevas donaciones a través de WebSocket', () => {
+    component.dataSource = new MatTableDataSource<UserData>([]);
+    const newDonation = {
+      newDonation: { Producto: 'Producto B', Cantidad: 5 },
+      user: { Nombre: 'Maria', Telefono: '87654321', Email: 'maria@ejemplo.com' },
+    };
+
+    component.handleNewDonation(newDonation);
+
+    expect(component.totalDonations).toBe(5);
+    expect(component.totalDonationsCount).toBe(1);
+    expect(component.averageDonations).toBe(5);
+    expect(component.dataSource.data.length).toBe(1);
+    expect(component.dataSource.data[0].name).toBe('Maria');
+    expect(component.dataSource.data[0].producto).toBe('Producto B');
+    expect(toastServiceMock.success).toHaveBeenCalledWith({
+      detail: 'EXITO',
+      summary: 'Nueva donación recibida',
+      duration: 5000,
+      position: 'topRight',
+    });
+  });
+
+  it('debería aplicar filtro a la tabla', () => {
+    const event = { target: { value: 'juan' } } as any;
+    component.dataSource.data = [
+      { name: 'Juan', telefono: '12345678', email: 'juan@ejemplo.com', producto: 'Producto A', cantidad: 10, progress: '' },
+      { name: 'Maria', telefono: '87654321', email: 'maria@ejemplo.com', producto: 'Producto B', cantidad: 5, progress: '' },
+    ];
+
+    component.applyFilter(event);
+
+    expect(component.dataSource.filter).toBe('juan');
+    expect(component.dataSource.filteredData.length).toBe(1);
+    expect(component.dataSource.filteredData[0].name).toBe('Juan');
+  });
+
+  it('debería manejar el error de carga de donaciones', (done) => {
+    const cuitFromStore = of('123456789');
+    const orgResponse = of({ id: 1 });
+
+    organizationServiceMock.getCuitFromStore.and.returnValue(cuitFromStore);
+    authServiceMock.getCuitFromToken.and.returnValue('123456789');
+    organizationServiceMock.getOrganizationByCuit.and.returnValue(orgResponse);
+    donationsServiceMock.getDonationsByOrganizationId.and.returnValue(throwError(() => new Error('Error de carga')));
+
+    spyOn(console, 'error');
+
+    component.loadDonations();
+
+    setTimeout(() => {
+      expect(console.error).toHaveBeenCalledWith('Error:', jasmine.any(Error));
+      // expect para verificar que existDonations esté en false
+      fixture.detectChanges();
+      expect(component.existDonations).toBe(false);
+      done();
+    }, 1000);
   });
 });
