d2d27a94c77f1d7c7cb49e33a4c34fce4a189b29
Erika <brenherrera@alumno.unlam.edu.ar>
2024-06-06 16:21:58 -0300

    fix test update-account

--- frontend/src/app/pages/update-account/update-account.component.spec.ts
+++ frontend/src/app/pages/update-account/update-account.component.spec.ts
@@ -1,17 +1,21 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { UpdateAccountComponent } from './update-account.component';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 describe('UpdateAccountComponent', () => {
   let component: UpdateAccountComponent;
   let fixture: ComponentFixture<UpdateAccountComponent>;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
       declarations: [UpdateAccountComponent]
     });
     fixture = TestBed.createComponent(UpdateAccountComponent);
     component = fixture.componentInstance;
+    httpMock = TestBed.inject(HttpTestingController);
     fixture.detectChanges();
   });
 
