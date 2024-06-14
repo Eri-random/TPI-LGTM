d7f5fc8a12b4543359c034e4bb2f33e406ffed89
cristianuehara <cuehara@alumno.unlam.edu.ar>
2024-06-14 00:54:00 -0300

    minor refactor

--- backend/backend.servicios/Servicios/MapsService.cs
+++ backend/backend.servicios/Servicios/MapsService.cs
@@ -1,30 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Http;
-using System.Text;
-using System.Threading.Tasks;
-using backend.servicios.Interfaces;
+﻿using backend.servicios.Interfaces;
 using Microsoft.Extensions.Configuration;
 using Newtonsoft.Json;
 
-
 namespace backend.servicios.Servicios
 {
-    public class MapsService: IMapsService
+    public class MapsService(IHttpClientFactory factory, IConfiguration configuration) : IMapsService
     {
-        private readonly HttpClient _httpClient;
-        private readonly string _googleMapsApiKey;
-
-        public MapsService()
-        {
-        }
-
-        public MapsService(HttpClient httpClient, IConfiguration configuration)
-        {
-            _httpClient = httpClient ?? throw new ArgumentNullException(nameof(httpClient));
-            _googleMapsApiKey = configuration["GoogleMapsApiKey"];
-        }
+        private readonly IHttpClientFactory _httpClientFactory = factory ?? throw new ArgumentNullException(nameof(factory));
+        private readonly string _googleMapsApiKey = configuration["GoogleMapsApiKey"];
 
         public async Task<(double lat, double lng)> GetCoordinates(string address, string city, string state)
         {
@@ -34,7 +17,8 @@ namespace backend.servicios.Servicios
 
             var url = $"https://maps.googleapis.com/maps/api/geocode/json?address={encodedAddress}+{encodedCity}+{encodedState}&key={_googleMapsApiKey}";
 
-            var response = await _httpClient.GetAsync(url);
+            var httpClient = _httpClientFactory.CreateClient();
+            var response = await httpClient.GetAsync(url);
             response.EnsureSuccessStatusCode();
 
             var json = await response.Content.ReadAsStringAsync();
