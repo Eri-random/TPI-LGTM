d2f85c5f75d40cda17e682fb26b4dbdecb56a66e
cristianuehara <cuehara@alumno.unlam.edu.ar>
2024-06-14 00:02:32 -0300

    apply repository to donation service

---
+++ backend/backend.repositories/interfaces/IRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace backend.repositories.interfaces
+{
+    public interface IRepository<T> where T : class
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T> GetByIdAsync(int id);
+        Task AddAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task UpdateRangeAsync(IEnumerable<T> entity);
+        Task DeleteAsync(int id);
+    }
+}
