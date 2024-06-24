ac46801a8e1a803035a3f8c2e11072e22b74bd66
Yair Oliva <yairolivait@gmail.com>
2024-06-23 21:41:55 -0300

    fix test front

--- frontend/src/app/interceptors/token.interceptor.spec.ts
+++ frontend/src/app/interceptors/token.interceptor.spec.ts
@@ -1,9 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 
 import { TokenInterceptor } from './token.interceptor';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 describe('TokenInterceptor', () => {
+  let httpMock : HttpTestingController;
+
   beforeEach(() => TestBed.configureTestingModule({
+    imports:[
+      HttpClientTestingModule,
+    ],
     providers: [
       TokenInterceptor
       ]
