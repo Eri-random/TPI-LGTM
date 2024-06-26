a27bc8c6144a3c7b3876de6ea686b36588133ea9
Tabatha Peralta <tabathapm@gmail.com>
2024-06-26 10:56:30 -0300

    test de PopupIdeaComponent

--- frontend/src/app/pages/generate-ideas/response-idea/popup-idea/popup-idea.component.spec.ts
+++ frontend/src/app/pages/generate-ideas/response-idea/popup-idea/popup-idea.component.spec.ts
@@ -1,22 +1,52 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { MatDialogRef } from '@angular/material/dialog';
-
+import { MatDialogRef, MAT_DIALOG_DATA, MatDialogModule } from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
 import { PopupIdeaComponent } from './popup-idea.component';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 describe('PopupIdeaComponent', () => {
-  let component: PopupIdeaComponent;
+  let componente: PopupIdeaComponent;
   let fixture: ComponentFixture<PopupIdeaComponent>;
+  let matDialogRefMock: any;
+
+  beforeEach(async () => {
+    matDialogRefMock = {
+      close: jasmine.createSpy('close')
+    };
+
+    await TestBed.configureTestingModule({
+      declarations: [PopupIdeaComponent],
+      imports: [MatDialogModule, MatIconModule],
+      providers: [
+        { provide: MatDialogRef, useValue: matDialogRefMock },
+        { provide: MAT_DIALOG_DATA, useValue: {} }
+      ],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA]
+    }).compileComponents();
+  });
 
   beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [PopupIdeaComponent]
-    });
     fixture = TestBed.createComponent(PopupIdeaComponent);
-    component = fixture.componentInstance;
+    componente = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('debería crear el componente', () => {
+    expect(componente).toBeTruthy();
+  });
+
+  it('debería cerrar el diálogo cuando se llama a onNoClick', () => {
+    componente.onNoClick();
+    expect(matDialogRefMock.close).toHaveBeenCalled();
+  });
+
+  it('debería cerrar el diálogo con "generateWithCurrentData" cuando se llama a onGenerateWithCurrentData', () => {
+    componente.onGenerateWithCurrentData();
+    expect(matDialogRefMock.close).toHaveBeenCalledWith('generateWithCurrentData');
+  });
+
+  it('debería cerrar el diálogo con "loadNewData" cuando se llama a onLoadNewData', () => {
+    componente.onLoadNewData();
+    expect(matDialogRefMock.close).toHaveBeenCalledWith('loadNewData');
   });
 });
